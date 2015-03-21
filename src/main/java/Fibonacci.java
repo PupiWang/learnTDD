51c6bfc252c467407374b5d31f96832d7abfa8f5
Pupi <dreamjl@live.cn>
2015-03-21 22:43:17 +0800

    fibonacci: the second test.

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,5 +1,6 @@
 public class Fibonacci {
-    public int fib(int n){
-        return 0;
+    public int fib(int n) {
+        if (n == 0) return 0;
+        return 1;
     }
 }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,10 +1,13 @@
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FibonacciTest {
     private Fibonacci fibonacci = new Fibonacci();
+
     @Test
     public void testFibonacci() {
-        Assert.assertEquals(0, fibonacci.fib(0));
+        assertEquals(0, fibonacci.fib(0));
+        assertEquals(1, fibonacci.fib(1));
     }
 }
