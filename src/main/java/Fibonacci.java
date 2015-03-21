784778dd52d8f5bac98978b1c9ccd5fcf98661b9
Pupi <dreamjl@live.cn>
2015-03-21 23:05:42 +0800

    fibonacci: refactor.

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,5 +1,5 @@
 public class Fibonacci {
-    public int fib(int n) {
+    public static int fib(int n) {
         if (n == 0) return 0;
         return 1;
     }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -3,11 +3,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FibonacciTest {
-    private Fibonacci fibonacci = new Fibonacci();
 
     @Test
     public void testFibonacci() {
-        assertEquals(0, fibonacci.fib(0));
-        assertEquals(1, fibonacci.fib(1));
+        int cases[][] = {{0,0},{1,1}};
+        for (int i = 0; i < cases.length; i++) {
+            assertEquals(cases[i][1], Fibonacci.fib(cases[i][0]));
+        }
     }
+
 }
